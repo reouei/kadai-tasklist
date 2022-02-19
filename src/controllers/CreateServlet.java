8324de95f33dce3ab7ca0776b68c98563c8aebd7
uei <uei@192.168.11.16>
2022-02-19 21:04:04 +0900

    「フラッシュメッセージを追加」

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -49,7 +49,9 @@ public class CreateServlet extends HttpServlet {
             m.setUpdated_at(currentTime);
             em.persist(m);
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");       // ここを追記
             em.close();
+
 
             response.sendRedirect(request.getContextPath() + "/index");
         }
